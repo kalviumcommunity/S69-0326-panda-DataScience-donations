926cef5d5079d93b1701fda4361e26561fc8e49c
karasi-sonica <karasisonica@gmail.com>
2026-05-11 16:28:34 +0530

    applied scaling

--- src/ml_training.py
+++ src/ml_training.py
@@ -1,5 +1,14 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/processed_donations.csv")
 
 print(df.head())
+
+X = df[['recency', 'frequency', 'total_amount', 'avg_amount']]
+y = df['will_donate_again']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+print(X_scaled[:5])
