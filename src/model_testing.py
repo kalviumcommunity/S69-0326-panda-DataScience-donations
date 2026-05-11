1fa655aafed8a79a2e035ff0808dd07023bc7a9e
bandlapraharshitha <praharshitha35@gmail.com>
2026-05-11 22:57:52 +0530

    test_cases

--- src/model_testing.py
+++ src/model_testing.py
@@ -4,3 +4,8 @@ import numpy as np
 
 model = pickle.load(open("models/donor_model.pkl", "rb"))
 scaler = pickle.load(open("models/scaler.pkl", "rb"))
+
+test_cases = [
+    [5, 8, 2000, 250],
+    [400, 1, 50, 50]
+]
