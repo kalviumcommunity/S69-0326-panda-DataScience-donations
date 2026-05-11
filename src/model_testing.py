b4d662d2a0f06c183a587a312c23e5251a21470a
bandlapraharshitha <praharshitha35@gmail.com>
2026-05-11 22:59:15 +0530

    prediction

--- src/model_testing.py
+++ src/model_testing.py
@@ -9,3 +9,15 @@ test_cases = [
     [5, 8, 2000, 250],
     [400, 1, 50, 50]
 ]
+
+for case in test_cases:
+
+    scaled = scaler.transform([case])
+    prediction = model.predict(scaled)
+
+    print("Input:", case)
+
+    if prediction[0] == 1:
+        print("Prediction: Will Donate Again")
+    else:
+        print("Prediction: Will Not Donate Again")
